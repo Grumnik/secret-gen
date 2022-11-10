7897ff37a3885ff69f8f36d2a5f232b4885d214b
mick <mick@highbiza.nl>
2022-11-10 21:41:13 +0100

    at least it is a start g

--- secret_gen/main.py
+++ secret_gen/main.py
@@ -1,6 +1,24 @@
+import argparse
+import random
+import string
+
+
 def main():
-    print("hello world")    
+    parser = argparse.ArgumentParser(description="Generate a random secret")
+    parser.add_argument(
+        "length", type=int, help="How many characters should the secret be?"
+    )
+    args = parser.parse_args()
+
+    chars = string.ascii_letters
+    nums = string.digits
+    specs = "!#$%&()*+,-.:;<=>?@[]_"
+    secret = []
+    for i in range(1, args.length):
+        secret.append(chars[random.randint(0, len(chars) - 1)])
+
+    print(secret)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
