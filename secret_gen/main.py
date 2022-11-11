df0d3ace888ed12bacda5bf49b68ed5fcafa3b1e
mick <mick@highbiza.nl>
2022-11-11 20:47:06 +0100

    hatsikidee

--- secret_gen/main.py
+++ secret_gen/main.py
@@ -1,22 +1,52 @@
 import argparse
 import random
 import string
+import math
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Generate a random secret")
-    parser.add_argument(
-        "length", type=int, help="How many characters should the secret be?"
-    )
-    args = parser.parse_args()
+    def get_args():
+        parser = argparse.ArgumentParser(description="Generate a random secret")
+        parser.add_argument(
+            "length", type=int, help="How many characters should the secret be?"
+        )
+        parser.add_argument(
+            "-n",
+            "--numbers",
+            default=10,
+            type=int,
+            help="numbers percentage of total length defaults to 10",
+        )
+        parser.add_argument(
+            "-s",
+            "--specials",
+            type=int,
+            default=10,
+            help="special character percentage of total length defaults to 10",
+        )
+        return parser.parse_args()
 
-    chars = string.ascii_letters
+    def randomizer(length, chars):
+        foo = []
+        for _ in range(0, length):
+            foo.append(chars[random.randint(-1, len(chars) - 1)])
+        return foo
+
+    args = get_args()
+    letters = string.ascii_letters
     nums = string.digits
     specs = "!#$%&()*+,-.:;<=>?@[]_"
-    secret = []
-    for i in range(1, args.length):
-        secret.append(chars[random.randint(0, len(chars) - 1)])
 
+    nums_length = math.ceil((args.length / 100) * args.numbers)
+    specs_length = math.ceil((args.length / 100) * args.specials)
+    letters_length = args.length - nums_length - specs_length
+
+    letters = randomizer(letters_length, letters)
+    nums = randomizer(nums_length, nums)
+    specs = randomizer(specs_length, specs)
+    chars = letters + nums + specs
+    random.shuffle(chars)
+    secret = "".join(chars)
     print(secret)
 
 
